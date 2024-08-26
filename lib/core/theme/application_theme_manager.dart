2b08fd888ce35cfe86c52a8f77c3740b6fbe342a
mohamed ahmed <m.ahmed20201863@gmail.com>
2024-08-26 20:39:46 +0300

    edit in home page

--- lib/core/theme/application_theme_manager.dart
+++ lib/core/theme/application_theme_manager.dart
@@ -3,43 +3,61 @@ import 'package:news_app/core/theme/color_palette.dart';
 
 class ApplicationThemeManager {
   static ThemeData lightThemeData = ThemeData(
-    primaryColor: ColorPalette.primaryColor,
-    textTheme: const TextTheme(
-      titleMedium: TextStyle(
-        fontSize: 22,
-        fontFamily: 'Exo',
-        fontWeight: FontWeight.w400,
+      primaryColor: ColorPalette.primaryColor,
+      textTheme: const TextTheme(
+        titleMedium: TextStyle(
+          fontSize: 22,
+          fontFamily: 'Exo',
+          fontWeight: FontWeight.w400,
+          color: ColorPalette.white,
+        ),
+        bodyLarge: TextStyle(
+          fontSize: 24,
+          fontFamily: 'Exo',
+          fontWeight: FontWeight.w700,
+          color: ColorPalette.white,
+        ),
+        bodyMedium: TextStyle(
+          fontSize: 22,
+          fontFamily: 'Exo',
+          fontWeight: FontWeight.w700,
+          color: ColorPalette.white,
+        ),
+        bodySmall: TextStyle(
+          fontSize: 22,
+          fontFamily: 'Exo',
+          fontWeight: FontWeight.w400,
+          color: ColorPalette.white,
+        ),
+        displaySmall: TextStyle(
+          fontSize: 10,
+          fontFamily: 'Exo',
+          color: ColorPalette.customGrey,
+        ),
+        displayMedium: TextStyle(
+          fontSize: 14,
+          fontFamily: 'Exo',
+          fontWeight: FontWeight.w400,
+          color: ColorPalette.black,
+        ),
+      ),
+      iconTheme: const IconThemeData(
         color: ColorPalette.white,
+        size: 35,
       ),
-      bodyLarge: TextStyle(
-        fontSize: 24,
-        fontFamily: 'Exo',
-        fontWeight: FontWeight.w700,
-        color: ColorPalette.white,
-      ),
-      bodyMedium: TextStyle(
-        fontSize: 22,
-        fontFamily: 'Exo',
-        fontWeight: FontWeight.w700,
-        color: ColorPalette.white,
-      ),
-      bodySmall: TextStyle(
-        fontSize: 22,
-        fontFamily: 'Exo',
-        fontWeight: FontWeight.w400,
-        color: ColorPalette.white,
-      ),
-      displaySmall: TextStyle(
-        fontSize: 10,
-        fontFamily: 'Exo',
-        color: ColorPalette.customGrey,
-      ),
-      displayMedium: TextStyle(
-        fontSize: 14,
-        fontFamily: 'Exo',
-        fontWeight: FontWeight.w400,
-        color: ColorPalette.black,
-      ),
-    ),
-  );
+      appBarTheme: const AppBarTheme(
+        backgroundColor: ColorPalette.primaryColor,
+        toolbarHeight: 40,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
+        ),
+        centerTitle: true,
+        iconTheme: IconThemeData(
+          color: ColorPalette.white,
+          size: 35,
+        ),
+      ));
 }
--- lib/features/home/pages/home_view.dart
+++ lib/features/home/pages/home_view.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:news_app/core/theme/color_palette.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var theme = Theme.of(context);
+    var mediaQuery = MediaQuery.of(context).size;
+    return Container(
+      decoration: const BoxDecoration(
+        color: ColorPalette.white,
+        image: DecorationImage(
+          image: AssetImage('assets/images/pattern.png'),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          toolbarHeight: mediaQuery.height * 0.11,
+          title: Text(
+            'News App',
+            style: theme.textTheme.titleMedium,
+          ),
+        ),
+        drawer: Drawer(),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                'Pick your category \n of interest.....',
+                style: theme.textTheme.bodyLarge!
+                    .copyWith(color: Colors.black, height: 1.2),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
