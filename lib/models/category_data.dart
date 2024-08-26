18f52a388f8f800b8c912752436a42eefca413c5
mohamed ahmed <m.ahmed20201863@gmail.com>
2024-08-26 20:42:19 +0300

    create CategoryData model

--- lib/models/category_data.dart
+++ lib/models/category_data.dart
@@ -1,1 +1,12 @@
-class CategoryData {}
+class CategoryData {
+  final String categoryId;
+  final String categoryName;
+  final String categoryIcon;
+  final String categoryBackGroundColor;
+
+  CategoryData(
+      {required this.categoryId,
+      required this.categoryName,
+      required this.categoryIcon,
+      required this.categoryBackGroundColor});
+}
