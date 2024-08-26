892c6de2de41e79aee0c7653d7342456d43176b6
mohamed ahmed <m.ahmed20201863@gmail.com>
2024-08-26 20:08:12 +0300

    add spalsh screen => spalsh_view.dart

--- lib/features/splash/pages/splash_view.dart
+++ lib/features/splash/pages/splash_view.dart
@@ -1,10 +1,41 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashView extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:news_app/core/config/page_route_name.dart';
+
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    Timer(
+      Duration(seconds: 2),
+      () => Navigator.pushReplacementNamed(context, PageRouteName.homeView),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        alignment: Alignment.center,
+        children: [
+          Image.asset(
+            'assets/images/pattern.png',
+            fit: BoxFit.cover,
+          ),
+          Image.asset(
+            'assets/icons/logo.png',
+            scale: 0.72,
+          )
+        ],
+      ),
+    );
   }
 }
